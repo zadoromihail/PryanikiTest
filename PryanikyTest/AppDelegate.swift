b1018e12218c4d6bcabfc30801b81c1305df11bf
Mihail <zadoromihail@gmail.com>
2021-03-05 12:41:58 +0300

    initial commit

---
+++ PryanikyTest/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  PryanikyTest
+//
+//  Created by Михаил Задорожный on 05.03.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let vc = ViewController()
+        let nc = UINavigationController(rootViewController: vc)
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        window.makeKeyAndVisible()
+        window.rootViewController = nc
+
+        return true
+    }
+}
+
---
+++ PryanikyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PryanikyTest
+//
+//  Created by Михаил Задорожный on 05.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Main"
+    }
+
+
+}
+
